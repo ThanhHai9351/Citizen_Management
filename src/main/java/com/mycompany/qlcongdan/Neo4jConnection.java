1d5f805cff86cd4dd13a02edea5edcd95aaa2eea
ThanhHai9351 <haihailua123456@gmail.com>
2024-10-14 23:37:34 +0700

    upload neo4jConnection

---
+++ src/main/java/com/mycompany/qlcongdan/Neo4jConnection.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.qlcongdan;
+
+/**
+ *
+ * @author Admin
+ */
+import org.neo4j.driver.*;
+
+public class Neo4jConnection {
+    private final Driver driver;
+
+    public Neo4jConnection() {
+        driver = GraphDatabase.driver("bolt://localhost:7687", AuthTokens.basic("ducngu", "123456"));
+    }
+
+    public Session getSession() {
+        return driver.session(); // Mở session mới
+    }
+
+    public void close() {
+        driver.close(); // Đóng driver khi không cần dùng nữa
+    }
+}
+
+
