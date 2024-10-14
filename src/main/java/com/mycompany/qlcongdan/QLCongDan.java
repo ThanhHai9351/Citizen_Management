cf6515a059932e49a4fca7e404063346c9f873ec
ThanhHai9351 <haihailua123456@gmail.com>
2024-10-14 14:42:05 +0700

    first commit

---
+++ src/main/java/com/mycompany/qlcongdan/QLCongDan.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.qlcongdan;
+
+/**
+ *
+ * @author pc
+ */
+public class QLCongDan {
+
+    public static void main(String[] args) {
+        Login frm = new Login();
+        frm.setVisible(true);
+    }
+}
